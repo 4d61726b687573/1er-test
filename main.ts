e18cc1ee8742ad728838de2136c2382c5fd06b63
4d61726b687573 <96146135+4d61726b687573@users.noreply.github.com>
2021-12-17 09:00:35 -0500

    Mettre à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        # # . # #
+        . . . . .
+        `)
+    basic.pause(100)
+}
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showString("Je m'appelle Marc")
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+        basic.pause(1500)
+    } else {
+        while (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+            basic.showString("Bonjour!")
+        }
+    }
 })
